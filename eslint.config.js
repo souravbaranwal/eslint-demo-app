0116bc2663452ea4f55c1285e38b6e1ebece4455
Sourav Kumar <sourav.baranwal@gmail.com>
2025-07-18 16:10:14 +0530

    integrated eslint, husky and lint staged

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,55 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+
+const prettierPlugin = require("eslint-plugin-prettier");
+const unusedImportsPlugin = require("eslint-plugin-unused-imports");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ignores: ["node_modules", "dist", ".expo", "eslint.config.js"],
+
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+        project: "./tsconfig.json",
+      },
+    },
+
+    plugins: {
+      prettier: prettierPlugin,
+      "unused-imports": unusedImportsPlugin,
+    },
+
+    rules: {
+      // 🔧 Prettier formatting (semi: false)
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          trailingComma: "all",
+          printWidth: 100,
+        },
+      ],
+
+      // 🧹 Remove unused imports and variables
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-unused-vars": "off",
+    },
   },
 ]);
